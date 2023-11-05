506e234a11eeb4c0b34a57c776559be62a999b94
mc-jazda <kgazda53@gmail.com>
2023-11-05 17:24:14 +0100

    Cpp function making bitmap grey

--- MandelbrotCpp/MandelCpp.cpp
+++ MandelbrotCpp/MandelCpp.cpp
@@ -1,8 +1,17 @@
 #include "pch.h"
 #include "MandelCpp.h"
 
-int generateMandelCpp(BYTE* bmp, int rowCount, int rowNum, int resX, int resY, int iterCount)
+void generateMandelCpp(BYTE* bmp, int rowCount, int rowNum, int resX, int resY, int iterCount)
 {
-	int test = resX + resY + iterCount + iterCount + int(bmp);
-	return test;
+	int alignment = (resX*3) % 4;
+
+	for (int y = 0; y < rowCount; y++)
+	{
+		for (int x = 0; x < resX*3; x++)
+		{
+			*bmp = 120;
+			bmp++;
+		}
+		bmp += alignment;
+	}
 }
