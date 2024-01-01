5ca5d9ce563287196887a3f32acc997076fd4b6d
mc-jazda <kgazda53@gmail.com>
2024-01-01 20:28:34 +0100

    Calculating Re(C)

--- MandelbrotMASM/MandelMASM.asm
+++ MandelbrotMASM/MandelMASM.asm
@@ -8,6 +8,7 @@
     y_start REAL4 -1.0
     y_end REAL4 1.0
     absMask DWORD 07FFFFFFFh            ; mask setting only sign bit to 0 - positive number
+    columnMask DWORD 0, 1, 2, 3, 4, 5, 6, 7
 
 .code
 
@@ -54,21 +55,39 @@ generateMandelMASM PROC ;bmp:QWORD, rowCount:DWORD, rowNum:DWORD, resX:DWORD, re
     vdivps ymm1, ymm1, ymm2             ; Scale Y - ymm1 = abs(y_end - y_start) / resY
 
     ;--------- Preparing for main loop --------- 
-    xor r10, r10                        ; current y of c - iterator
+    xor r10, r10                        ; current y of C - iterator
     movd xmm8, r8d                      ; moving rowNum as scalar to xmm8
     vbroadcastss ymm8, xmm8             ; broadcasting rowNum to ymm8
-    vbroadcastss ymm9, dword ptr [y_start] ; broadcasting y_start to ymm9
+    vbroadcastss ymm9, dword ptr [y_start]  ; broadcasting y_start to ymm9
+    vbroadcastss ymm10, dword ptr [x_start] ; broadcasting x_start to ymm10
+    vmovdqu ymm11, ymmword ptr [columnMask] ; load 0, 1, 2, 3, 4, 5, 6, 7 to ymm11 - needed for calculating Re(C)
+    vcvtdq2ps ymm11, ymm11                  ; convert DWORD to REAL4
 
 
     LOOP_ROWS:
         ;--------- Calculating Im(C) --------- 
-        movd xmm2, r10d                     ; moving current y as scalar to xmm2
-        vbroadcastss ymm2, xmm2             ; broadcasting y to ymm2
+        movd xmm2, r10d                     ; moving current Y as scalar to xmm2
+        vbroadcastss ymm2, xmm2             ; broadcasting Y to ymm2
         vcvtdq2ps ymm2, ymm2                ; convert DWORD to REAL4
         vaddps ymm2, ymm2, ymm8             ; ymm2 = rowNum + y
         vmulps ymm2, ymm2, ymm1             ; ymm2 = (rowNum + y) * scaleY
         vaddps ymm2, ymm2, ymm9             ; Im(C) - ymm2 = (rowNum + y) * yScale + yStart
 
+        xor r11, r11                        ; current x of C - iterator
+
+        LOOP_COLUMNS:
+            ;--------- Calculating Re(C) ---------
+            movd xmm3, r11d                     ; moving current X as scalar to xmm3
+            vbroadcastss ymm3, xmm3             ; broadcasting X to ymm3
+            vcvtdq2ps ymm3, ymm3                ; convert DWORD to REAL4
+            vaddps ymm3, ymm3, ymm11            ; adding columnMask - makes every value greater than previous one by 1
+            vmulps ymm3, ymm3, ymm0             ; ymm3 = x * xScale
+            vaddps ymm3, ymm3, ymm10            ; Re(C) - ymm3 = x * xScale + xStart
+
+
+            add r11d, 8
+            cmp r11d, r9d
+            jl LOOP_COLUMNS
 
 
         inc r10d
